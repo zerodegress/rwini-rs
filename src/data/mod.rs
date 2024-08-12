14bf9bad7d05b2e0c0be6d357b59805594328c0d
zerodegress <zerodegress@outlook.com>
2024-08-12 23:09:34 +0800

    Fixed: test

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -12,7 +12,7 @@ mod test {
   fn test_ini_to_string() {
     assert_eq!(
       (Into::<Ini>::into([("core", [("name", "1"), ("key", "value"),]).into()])).to_string(),
-      "[core]\nname:1\nkey:value"
+      "[core]\nkey:value\nname:1"
     )
   }
 }
